2c394c7ede7a44ceafd3c17935c49dd29f93be97
Federico Mederos <fedemederos@MacBook-Pro-de-Federico.local>
2024-03-08 21:03:58 -0300

    interfaz iniciada

---
+++ app.js
@@ -0,0 +1,21 @@
+// Definiendo funciones para Operadores
+
+// Suma
+function suma(a, b) {
+    return a + b;
+}
+
+// Resta
+function resta(a, b) {
+    return a - b;
+}
+
+// Multiplicación
+function multi(a, b) {
+    return a * b;
+}
+
+// División
+function division(a, b) {
+    return a / b;
+}
